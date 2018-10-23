5c809e45c47db528b4ee71d50229e2c6abd18cd4
bluedread <patrick@nietvergeten.nl>
2018-10-23 16:53:13 +0200

    refactored: duplicate code eliminated

--- manifests/basic_entry.pp
+++ manifests/basic_entry.pp
@@ -9,25 +9,20 @@ define basicauth::basic_entry(
     include ::stdlib
 
     case $algorithm  {
-        default: {
-            # this includes the 'literal' option
-            concat::fragment{ "basicauth_fragment_${user}":
-                # always the same location as the main class
-                target  => $::basicauth::location,
-                order   => '10',
-                content => "${user}:${password}\n",
-            }
-        }
         'hash': {
             $seed            = fqdn_rand_string(10, '', '4dKqzRr4XUBzL2QD8hLw')
             $hashed_password = pw_hash($password, $hashtype, $seed)
-
-            concat::fragment{ "basicauth_fragment_${user}":
-                # always the same location as the main class
-                target  => $::basicauth::location,
-                order   => '10',
-                content => "${user}:${hashed_password},\n",
-            }
+            $content         = "${user}:${hashed_password},\n"
         }
+        default: {
+            $content         = "${user}:${password}\n"
+        }
+    }
+
+    concat::fragment{ "basicauth_fragment_${user}":
+        # always the same location as the main class
+        target  => $::basicauth::location,
+        order   => '10',
+        content => $content,
     }
 }
